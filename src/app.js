b1d11bc97bb12487a45a63ae856015b7cf4587ca
young.bae <young.bae@woven-planet.global>
2021-04-15 16:07:53 +0900

    feat: Create tiles

--- src/app.js
+++ src/app.js
@@ -74,7 +74,13 @@ dinoCompareForm.onsubmit = (e) => {
       throw new Error('weights are both needed to compare');
     }
 
-    return dinoWeight > humanWeight;
+    if (dinoWeight > humanWeight) {
+      return 'heavier';
+    } else if (dinoWeight < humanWeight) {
+      return 'lighter';
+    }
+
+    return 'has the same weight';
   }
 
   // Create Dino Compare Method 2
@@ -84,7 +90,13 @@ dinoCompareForm.onsubmit = (e) => {
       throw new Error('heights are both needed to compare');
     }
 
-    return dinoWeight > humanWeight;
+    if (dinoHeight > humanHeight) {
+      return 'taller';
+    } else if (dinoHeight < humanHeight) {
+      return 'smaller';
+    }
+
+    return 'has the same height';
   }
 
   // Create Dino Compare Method 3
@@ -94,12 +106,54 @@ dinoCompareForm.onsubmit = (e) => {
       throw new Error('diets are both needed to compare');
     }
 
-    return dinoDiet > humanDiet;
+    if (dinoDiet !== humanDiet) {
+      return 'has a different diet';
+    }
+
+    return 'has the same diet';
   }
 
   // Generate Tiles for each Dino in Array
+  const tiles = dinos.map(
+    ({diet, weight, height, species, where, when, fact}) => {
+      const itemEl = document.createElement('div');
+      itemEl.className = 'grid-item';
+
+      // Each Dino title must include at least the species, an image and a fact.
+      // The dino fact displayed should be chosen at random from at least 6 options
+      // (including your 3 methods).
+      const titleEl = document.createElement('h3');
+      titleEl.innerText = species;
+
+      const imgEl = document.createElement('img');
+      imgEl.src = `images/${species
+        .trim()
+        .split(' ')
+        .join('')
+        .toLowerCase()}.png`;
+      imgEl.alt = `image of ${species}`;
+
+      const textEl = document.createElement('p');
+
+      textEl.innerHTML = `${fact}<br />Compare from ${
+        human.name
+      }: ${compareWeight(weight, human.weight)}, ${compareHeight(
+        height,
+        human.height
+      )}, ${compareDiet(diet, human.diet)}`;
+
+      itemEl.append(titleEl, imgEl, textEl);
+
+      return itemEl;
+    }
+  );
+
+  console.log('tiles: ', tiles);
 
   // Add tiles to DOM
+  const gridEl = document.getElementById('grid');
+  gridEl.append(...tiles);
 
   // Remove form from screen
+  dinoCompareForm.style.display = 'none';
 };
