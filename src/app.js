c8bfdb36ce797f52a776100fb9184b1d181917fa
young.bae <young.bae@woven-planet.global>
2021-04-15 16:33:08 +0900

    feat: Create human tile, shuffle array

--- src/app.js
+++ src/app.js
@@ -114,45 +114,61 @@ dinoCompareForm.onsubmit = (e) => {
   }
 
   // Generate Tiles for each Dino in Array
-  const tiles = dinos.map(
-    ({diet, weight, height, species, where, when, fact}) => {
-      const itemEl = document.createElement('div');
-      itemEl.className = 'grid-item';
+  const tiles = dinos.map(({diet, weight, height, species, fact}) => {
+    const itemEl = document.createElement('div');
+    itemEl.className = 'grid-item';
 
-      // Each Dino title must include at least the species, an image and a fact.
-      // The dino fact displayed should be chosen at random from at least 6 options
-      // (including your 3 methods).
-      const titleEl = document.createElement('h3');
-      titleEl.innerText = species;
+    // Each Dino title must include at least the species, an image and a fact.
+    // The dino fact displayed should be chosen at random from at least 6 options
+    // (including your 3 methods).
+    const titleEl = document.createElement('h3');
+    titleEl.innerText = species;
 
-      const imgEl = document.createElement('img');
-      imgEl.src = `images/${species
-        .trim()
-        .split(' ')
-        .join('')
-        .toLowerCase()}.png`;
-      imgEl.alt = `image of ${species}`;
+    const imgEl = document.createElement('img');
+    imgEl.src = `images/${species
+      .trim()
+      .split(' ')
+      .join('')
+      .toLowerCase()}.png`;
+    imgEl.alt = `image of ${species}`;
 
-      const textEl = document.createElement('p');
+    const textEl = document.createElement('p');
 
-      textEl.innerHTML = `${fact}<br />Compare from ${
-        human.name
-      }: ${compareWeight(weight, human.weight)}, ${compareHeight(
-        height,
-        human.height
-      )}, ${compareDiet(diet, human.diet)}`;
+    textEl.innerHTML = `${fact}<br />Compare from ${
+      human.name
+    }: ${compareWeight(weight, human.weight)}, ${compareHeight(
+      height,
+      human.height
+    )}, ${compareDiet(diet, human.diet)}`;
 
-      itemEl.append(titleEl, imgEl, textEl);
+    itemEl.append(titleEl, imgEl, textEl);
 
-      return itemEl;
-    }
-  );
+    return itemEl;
+  });
 
-  console.log('tiles: ', tiles);
+  const humanTile = (function () {
+    const {name} = human;
+
+    const itemEl = document.createElement('div');
+    itemEl.className = 'grid-item';
+
+    const titleEl = document.createElement('h3');
+    titleEl.innerText = name;
+
+    const imgEl = document.createElement('img');
+    imgEl.src = 'images/me.png';
+    imgEl.alt = `image of ${name}`;
+
+    itemEl.append(titleEl, imgEl);
+
+    return itemEl;
+  })();
+
+  tiles.push(humanTile);
 
   // Add tiles to DOM
   const gridEl = document.getElementById('grid');
-  gridEl.append(...tiles);
+  gridEl.append(...tiles.sort(() => Math.random() - 0.5));
 
   // Remove form from screen
   dinoCompareForm.style.display = 'none';
